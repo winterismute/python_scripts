a8b7d844353e142f95bd2c2745ebbbc6bd1527c4
Chris Sinclair <chris@quarterstaff.co.uk>
2015-10-17 13:56:54 +0100

    Picked a side on single vs. double quoting strings

--- duo_to_graphviz.py
+++ duo_to_graphviz.py
@@ -19,12 +19,12 @@ def get_file_data(f):
     return json.load(fp)
 
 def parse_json(data,source,dest,phases):
-    colours = {1:"red", 2:"yellow", 3:"green"}
+    colours = {1:'red', 2:'yellow', 3:'green'}
 
     course_data = filter(lambda cd: cd[0] in phases and
-                                    (data["languages"][cd[1]]["name"].upper() == source or not source) and
-                                    (data["languages"][cd[2]]["name"].upper() == dest or not dest),
-                                    [(a["phase"],a["from_language_id"],a["learning_language_id"]) for a in data["directions"]])
+                                    (data['languages'][cd[1]]['name'].upper() == source or not source) and
+                                    (data['languages'][cd[2]]['name'].upper() == dest or not dest),
+                                    [(a['phase'],a['from_language_id'],a['learning_language_id']) for a in data['directions']])
 
     courses = {}
     for (phase, from_lang, to_lang) in sorted(course_data):
@@ -34,28 +34,28 @@ def parse_json(data,source,dest,phases):
             courses[phase][from_lang] = []
         courses[phase][from_lang].append(to_lang)
 
-    print("digraph G {")
-    print("  rankdir=LR;")
-    print("  overlap=false;")
+    print('digraph G {')
+    print('  rankdir=LR;')
+    print('  overlap=false;')
 
     for phase in courses.keys():
-        print("\n  edge [color={}]".format(colours[phase]))
+        print('\n  edge [color={}]'.format(colours[phase]))
 
         for from_lang in sorted(courses[phase]):
-            print("  \"{}\" -> {{ ".format(data["languages"][from_lang]["name"]), end="")
+            print('  "{}" -> {{ '.format(data['languages'][from_lang]['name']), end='')
             for to_lang in sorted(courses[phase][from_lang]):
-                print("\"{}\" ".format(data["languages"][to_lang]["name"]), end="")
+                print('"{}" '.format(data['languages'][to_lang]['name']), end='')
 
-            print("};")
-    print("}")
+            print('};')
+    print('}')
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='Process Duolingo course data into a dot file for graphviz')
-    parser.add_argument('filename', nargs="?", help="Name of the file with the Duolingo course data. Requests current data from the Duolino API if ommitted")
-    parser.add_argument('-s','--source_language', default="", help="Filter to only show courses from the SOURCE_LANGUAGE")
-    parser.add_argument('-d','--dest_language', default="", help="Filter to only show courses to the DEST_LANGUAGE")
-    parser.add_argument('-p','--phase', nargs="*", type=int, default=[1,2,3], choices=[1,2,3], help="Only show courses in the selected phase(s)")
+    parser.add_argument('filename', nargs='?', help='Name of the file with the Duolingo course data. Requests current data from the Duolino API if ommitted')
+    parser.add_argument('-s','--source_language', default='', help='Filter to only show courses from the SOURCE_LANGUAGE')
+    parser.add_argument('-d','--dest_language', default='', help='Filter to only show courses to the DEST_LANGUAGE')
+    parser.add_argument('-p','--phase', nargs='*', type=int, default=[1,2,3], choices=[1,2,3], help='Only show courses in the selected phase(s)')
     args = parser.parse_args()
 
     if args.filename:
