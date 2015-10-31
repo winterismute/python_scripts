261f127f45d853d8c468dc8185597eb78540382a
Chris Sinclair <chris@quarterstaff.co.uk>
2015-10-31 11:25:52 +0000

    Reorganised to remove global variables

--- duo_to_graphviz.py
+++ duo_to_graphviz.py
@@ -28,7 +28,7 @@ def get_file_data(f):
       data = json.load(fp)
     return data
 
-def filter_languages(data, source, dest, phases):
+def filter_languages(data, languages, source, dest, phases):
 
     include_sources = [l for l in source if l[0] != '~']
     exclude_sources = [l[1:] for l in source if l[0] == '~']
@@ -37,14 +37,14 @@ def filter_languages(data, source, dest, phases):
     exclude_destinations = [l[1:] for l in dest if l[0] == '~']
 
     course_data = filter(lambda cd: cd[0] in phases and
-                             ((len(include_sources) == 0 or data['languages'][cd[1]]['name'].upper() in include_sources) and
-                              (len(exclude_sources) == 0 or data['languages'][cd[1]]['name'].upper() not in exclude_sources)) and
-                             ((len(include_destinations) == 0 or data['languages'][cd[2]]['name'].upper() in include_destinations) and
-                              (len(exclude_destinations) == 0 or data['languages'][cd[2]]['name'].upper() not in exclude_destinations)),
-                             [(a['phase'],a['from_language_id'],a['learning_language_id']) for a in data['directions']])
+                             ((len(include_sources) == 0 or languages[cd[1]].upper() in include_sources) and
+                              (len(exclude_sources) == 0 or languages[cd[1]].upper() not in exclude_sources)) and
+                             ((len(include_destinations) == 0 or languages[cd[2]].upper() in include_destinations) and
+                              (len(exclude_destinations) == 0 or languages[cd[2]].upper() not in exclude_destinations)),
+                             data)
     return course_data
 
-def parse_json(course_data, colours):
+def parse_json(course_data, languages, colours):
 
     courses = {}
     for (phase, from_lang, to_lang) in sorted(course_data):
@@ -62,14 +62,14 @@ def parse_json(course_data, colours):
         print('\n  edge [color={}]'.format(colours[phase]))
 
         for from_lang in sorted(courses[phase]):
-            print('  "{}" -> {{ '.format(data['languages'][from_lang]['name']), end='')
+            print('  "{}" -> {{ '.format(languages[from_lang]), end='')
             for to_lang in sorted(courses[phase][from_lang]):
-                print('"{}" '.format(data['languages'][to_lang]['name']), end='')
+                print('"{}" '.format(languages[to_lang]), end='')
 
             print('};')
     print('}')
 
-if __name__ == '__main__':
+def get_arguments():
     import argparse
     parser = argparse.ArgumentParser(description='Process Duolingo course data into a dot file for graphviz')
     parser.add_argument('filename', nargs='?', help='Name of the file with the Duolingo course data. Requests current data from the Duolino API if ommitted')
@@ -78,7 +78,10 @@ if __name__ == '__main__':
     parser.add_argument('-p','--phase', nargs='*', type=int, default=[1,2,3], choices=[1,2,3], help='Only show courses in the selected phase(s)')
     parser.add_argument('--download', default='', action='store_const', const='Y', help='Download and display the API data for easy output to a file')
     parser.add_argument('-c','--colours', nargs=3, type=str, default=['red','yellow','green'], help='Choose alternate colours for phase 1, 2 and 3')
-    args = parser.parse_args()
+    return parser.parse_args()
+
+def main():
+    args = get_arguments()
 
     data = None
     if args.download == 'Y' or not args.filename:
@@ -91,9 +94,18 @@ if __name__ == '__main__':
       print(data.text)
 
     else:
-        course_data = filter_languages(data,
+        languages = {code:data['languages'][code]['name'] for code in data['languages']}
+
+        course_data = [(direction['phase'],direction['from_language_id'],direction['learning_language_id']) for direction in data['directions']]
+        course_data = filter_languages(course_data,
+                            languages,
                             list(map(str.upper, args.source_language)),
                             list(map(str.upper, args.dest_language)),
                             args.phase)
-        edges = {phase:colour for (phase, colour) in zip([1,2,3], args.colours)}
-        parse_json(course_data, edges)
+
+        colours = {phase:colour for (phase, colour) in zip([1,2,3], args.colours)}
+
+        parse_json(course_data, languages, colours)
+
+if __name__ == '__main__':
+    main()
